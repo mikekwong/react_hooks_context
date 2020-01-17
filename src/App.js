f5f65c97bb3f811df36596f7f10c003971bfcec2
Mike Wong <wongmdesign@gmail.com>
2020-01-17 13:21:54 -0500

    fetching working with context

--- src/App.js
+++ src/App.js
@@ -24,27 +24,27 @@ export default function App () {
     <>
       {console.log(state)}
       <div>
-        <h1>Rick and Morty</h1>
+        <h1 className='header'>Rick and Morty</h1>
         <p>Pick your favorite episodes</p>
-        <section>
-          {state.episodes.map(episode => {
-            return (
-              <section key={episode.id}>
-                <img
-                  src={episode.image.medium}
-                  alt={`Rick and Morty ${episode.name}`}
-                />
-                <div>{episode.name}</div>
-                <section>
-                  <div>
-                    Season: {episode.season} Number: {episode.number}
-                  </div>
-                </section>
-              </section>
-            )
-          })}
-        </section>
       </div>
+      <section className='episode-layout'>
+        {state.episodes.map(episode => {
+          return (
+            <section key={episode.id} className='episode-box'>
+              <img
+                src={episode.image.medium}
+                alt={`Rick and Morty ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                <div>
+                  Season: {episode.season} Number: {episode.number}
+                </div>
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </>
   )
 }
